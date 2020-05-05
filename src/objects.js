238aae2534151cab5aca916649bc57b9d3df0efd
“Joe <josephbramwell@bolstersystems.com>
2020-05-05 16:11:12 +0100

    all complete

--- src/objects.js
+++ src/objects.js
@@ -1,42 +1,54 @@
 const createPerson = (name, age) => {
-  // your code here
+ return { name, age }
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object.age;
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return !!object[property];
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  } else { 
+    return false;
+  }
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(hondaCars => hondaCars.manufacturer === "Honda");
 };
 
 const averageAge = people => {
-  // your code here
+  return people.reduce((accumulator,person) => accumulator + person.age, 0) / people.length
+
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
-};
+  const person = {
+    name: name,
+    age: age,
+    introduce(newPersonsName) {
+      return `Hi ${newPersonsName}, my name is ${name} and I am ${age}!`
+    }
+  }; return person;
+  };
+
 
 module.exports = {
   createPerson,
